047b32adda347a69b3e951ab1ee4b98fb24756b7
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2022-10-14 15:38:03 -0700

    [+] postgresql

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
-const express = require('express');
+import express from 'express';
+import postgresql from 'postgresql';
+
+postgresql();
 const app = express();
 
 app.get('/', (req, res) => res.send('My first REST API!'));
 
+app.get('/test', async (req, res) => {
+  res.status(200).send(JSON.stringify("Test success"));
+});
+
 const port = 8000;
+
 app.listen(port, () => {
   console.log('Listening on port ' + port);
 });
